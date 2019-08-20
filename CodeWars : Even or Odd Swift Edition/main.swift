c4723aaf910a087a3f574051b3a9e580eb092c5b
Tanatip Denduangchai <De.tanatip@outlook.com>
2019-08-20 12:56:48 +0700

    Initial Commit

---
+++ CodeWars : Even or Odd Swift Edition/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CodeWars : Even or Odd Swift Edition
+//
+//  Created by Tanatip Denduangchai on 8/20/19.
+//  Copyright © 2019 Tanatip Denduangchai. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
